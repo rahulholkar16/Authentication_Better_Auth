7c7b3c6348a5df6e0716de40c774766d9e30fb70
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-07 20:15:45 +0530

    Setupt Better Auth

---
+++ lib/auth.ts
@@ -0,0 +1,16 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(db, {
+        provider: "postgresql"
+    }),
+    emailAndPassword: {
+        enabled: true
+    },
+    socialProviders: {
+        github: {
+            
+        }
+    }
+});
