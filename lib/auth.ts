4c966668e62c4a7ceeffeba24f59594cbb910a8c
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-08 17:58:49 +0530

    Add better auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { db } from "./db";
 
 export const auth = betterAuth({
     database: prismaAdapter(db, {
