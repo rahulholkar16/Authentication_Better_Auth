9af3be81c26e2b26f555cf083d43e024277b44f3
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-08 20:36:24 +0530

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
-import { Button } from "@/components/ui/button";
+import HomeView from "@/components/HomeView";
+import { auth } from "@/lib/auth";
 
-export default function Home() {
-  return (
-    <Button variant={"outline"} className={"bg-black text-white"}>Click</Button>
-  );
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (!session) {
+        redirect("/login");
+    }
+
+    return (
+        <div>
+            <HomeView />
+        </div>
+    );
 }
